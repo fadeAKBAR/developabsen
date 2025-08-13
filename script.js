13183e19722300c6ff48264c471d5668afbfe81d
fadeAKBAR <66496567+fadeAKBAR@users.noreply.github.com>
2025-08-13 23:21:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Ganti dengan URL API Anda jika berubah
-const API_URL = "https://script.google.com/macros/s/AKfycbz3b0pBBiK7aJSxsxWXV_EzxojTP4OUNsLGwjogj-iSKnUAG5S1ImQEcfqgZrwOTk0/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzuk4NQVpK7es_d7qF9zX_svNiDFaR8SBjifBKmgUlZkjvvOnfaXqU8CECqaStJG6wJ/exec";
 
 let currentUser = null;
 
